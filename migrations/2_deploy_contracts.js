253006340c61636622b5996f2c3c26f7390df830
Dome Charoenyos <dome@tel.co.th>
2020-05-31 23:44:03 +0700

    first commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var ThaichainStable = artifacts.require ("TCSImplementationV2.sol");
+module.exports = function (deployer) {
+    deployer.deploy (ThaichainStable);
+};
+
---
+++ truffle-config.js
@@ -0,0 +1,57 @@
+const privateKey = process.env.WEB3_PRIVATE_KEY;
+var HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic = "<your-mnemonic>";
+const walletChildNum = 0;
+const networkAddressMainnet = "https://mainnet.infura.io/v3/<your-api-key>";
+const networkAddressTestnet = "https://ropsten.infura.io/v3/<your-api-key>";
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 8545, // ganache-cli
+      network_id: '*', // Match any network id
+      gas: 6700000,
+      gasPrice: 0x01
+    },
+    coverage: {
+      host: 'localhost',
+      network_id: '*',
+      port: 8321,
+      gas: 10000000000000,
+      gasPrice: 0x01
+    },
+    mainnet: {
+      network_id: 1,
+      provider: function() {
+        return new HDWalletProvider(mnemonic, networkAddressMainnet, walletChildNum)
+      },
+    },
+    thaichain: {
+      provider: () => new HDWalletProvider(privateKey, "https://rpc.dome.cloud"),
+      network_id: "*", // Match any network id
+      gas: 2e7,
+      gasPrice: 4e9
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, networkAddressTestnet, walletChildNum)
+      },
+      network_id: 3,
+      gas: 4000000
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
+  compilers: {
+    solc: {
+      version: "v0.4.24+commit.e67f0147" // Freeze solidity version 
+    }
+  }
+};
